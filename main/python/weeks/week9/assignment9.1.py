d974327eb0665b01cdccc5356b03d4eaae13ecd0
Inna Yankovich <innay@compatibl.com>
2014-11-27 16:31:40 +0200

    week 8

---
+++ main/python/weeks/week9/assignment9.1.py
@@ -0,0 +1,34 @@
+from pip.backwardcompat import raw_input
+
+__author__ = 'Sergii'
+# Write a program to read through the mbox-short.txt and figure out who has the sent the greatest number of mail
+# messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent
+# the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number
+# of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using
+# a maximum loop to find the most prolific committer.
+
+name = raw_input("Enter file:")
+if len(name.strip()) < 1:
+    name = 'mbox-short.txt'
+fileHandler = open(name)
+emailList = list()
+
+for line in fileHandler:
+        if not line.startswith("From") or line.startswith("From:"):
+            continue
+        words = line.split()
+        emailList.append(words[1])
+emailDict = dict()
+for email in emailList:
+    emailDict[email] = emailDict.get(email, 0) + 1
+
+popularEmail = None
+popularEmailCount = None
+
+for email, count in emailDict.items():
+    if popularEmailCount is None or count > popularEmailCount:
+        popularEmail = email
+        popularEmailCount = count
+print(popularEmail, popularEmailCount)
+
+
